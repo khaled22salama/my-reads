8e05224fec3f40cd8e12b3864fdb8c003cd79132
Khaled Salama <khaled.love2016@outlook.com>
2019-01-28 09:01:31 +0200

    Add files via upload

--- src/screens/Search/Search.jsx
+++ src/screens/Search/Search.jsx
@@ -13,29 +13,23 @@ export default class Search extends Component {
 			allBooks: []
 		}
 	}
-
 	componentDidMount() {
 		this.fetchBooks()
 	}
-
 	fetchBooks() {
 		getAll().then((data) => {
 			console.log('all books loaded')
 			this.setState({ allBooks: data })
 		})
 	}
-
 	setSearchState(books, curReq){
 		if (curReq < this.state.maxReq){
 			console.log('a response was invalidated')
-			// old request, invalidate
 			return;
 		}
 		this.setState({books: books, maxReq: curReq})
 	}
-
 	doSearch(term){
-		// attaching IDs to request, to solve async stale data issues
 		let curReq = this.state.reqCount + 1
 		this.setState({reqCount: curReq})
 	
@@ -44,27 +38,21 @@ export default class Search extends Component {
 			return;
 		}
 		search(term).then((books) => {
-			console.log('search done')  // again takes a lot of time so
+			console.log('search done') 
 			this.setSearchState(books, curReq)
 		}).catch (() => {
 			console.log('search failed')
 			this.setSearchState([], curReq)
-			// happens sometimes, maybe on invalid terms
-			// REQ: Invalid queries are handled and prior search results are not shown.
 		})
 	}
-
 	inputChange(evt){
 		const term = evt.target.value.trim()
 		this.doSearch(term)
 	}
-
 	updateHandler(book, shelf) {
 		this.updateBook(book, shelf)
 		update(book, shelf).then(() => console.log('Book update done'))
-		// ^ takes a lot of time so better for checking
 	}
-
 	updateBook(book, shelf) {
 		let books = this.state.allBooks
 		let found = false
@@ -75,14 +63,12 @@ export default class Search extends Component {
 			}
 		})
 		if (!found){
-			// add new book
-			books[book.id] = JSON.parse(JSON.stringify(book)) // clone
+		//new book
+			books[book.id] = JSON.parse(JSON.stringify(book))
 			books[book.id].shelf = shelf
 		}
 		this.setState({ allBooks: books })
 	}
-
-	// gets book from allBooks collection
 	getBook(searchBook){
 		let books = this.state.allBooks;
 		for (let key in books){
@@ -93,7 +79,6 @@ export default class Search extends Component {
 		}
 		return searchBook
 	}
-
 	render() {
 		let books = this.state.books.map((book) => (
 			<Book key={book.id} {...this.getBook(book)} handler={this.updateHandler.bind(this)} />
